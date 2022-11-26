1df29aaa28f5e60bff14d069416f600decac1fd8
shoppingzh <xpzheng6@163.com>
2022-11-26 16:24:16 +0800

    feat: mul util

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,7 @@ export function plus(a: number, b: number) {
   if (!isNumber(a) || !isNumber(b)) throw new Error('is not number')
   return a + b
 }
+
+export function mul(a: number, b: number) {
+  return a * b
+}
